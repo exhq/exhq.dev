e059223bdb67ccad5b63b8d547e23818ae838bc6
echo <91651232+exhq@users.noreply.github.com>
2022-10-03 17:55:39 +0330

    I LIED!!!! MUHAHAHAHAHHAHAHAHAHAHA

--- static/funny.js
+++ static/funny.js
@@ -1,1 +1,16 @@
-//nothing :) **FOR NOW**
+bool = false
+var audio 
+if (new Date().getMonth() === 9){
+    console.log("boo")
+    audio = new Audio('boo.mp3')
+} else {
+    audio = new Audio('for-maeve.mp3')
+}
+function no() {
+    bool = !bool
+    if(bool){
+        audio.play()
+    } else {
+        audio.pause()
+    }
+  }

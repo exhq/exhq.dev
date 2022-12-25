e47c564820a5bbdf431403d4edf93805ad45baf1
echo <91651232+exhq@users.noreply.github.com>
2022-12-25 16:20:55 +0330

    lmao

--- lmao.js
+++ lmao.js
@@ -1,3 +1,16 @@
+let no = document.getElementById("sotrue");
+no.innerHTML = `<div id="sotrue" class="flex-c-1">
+<div class="flex-c-r">
+  <img onclick="bruh()"
+    src="https://dp.nea.moe/avatar/712639419785412668.png"
+    alt="clientpicture"
+    class="pfp"
+  />
+  <h1 onclick="lmao()" >ECHO</h1>
+</div>
+<p>shitty programmer | MtF | they/them</p>
+</div>`;
+
 let bool;
 let beat = new Audio("./music.mp3");
 function lmao() {
@@ -7,4 +20,40 @@ function lmao() {
   } else {
     beat.pause();
   }
+}
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+async function load(url) {
+  return await (await fetch(url)).json();
+}
+async function bruh() {
+  load(
+    "https://literallyafuckingjsonfile.literallyafuckingjsonfile.workers.dev/"
+  ).then((data) => {
+    const funnyLaughingDiscordMessageToDisplayIEnjoy =
+      data[getRandomInt(0, data.length - 1)];
+
+    fetch(
+      `https://dp.nea.moe/avatar/${funnyLaughingDiscordMessageToDisplayIEnjoy.from}.json`
+    )
+      .then((it) => it.json())
+      .then((authorData) => {
+        const dm = document.getElementById("sotrue");
+        dm.innerHTML = `<div id="sotrue" class="flex-c-1">
+        <div class="flex-c-r">
+          <img onclick="bruh()"
+            src="${authorData.avatar}"
+            alt="clientpicture"
+            class="pfp"
+          />
+          <h1 onclick="lmao()" >${authorData.username}</h1>
+        </div>
+        <p>${funnyLaughingDiscordMessageToDisplayIEnjoy.text}</p>
+        </div>`;
+        twemoji.parse(dm);
+      });
+  });
 }

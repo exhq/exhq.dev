2985f34e85f450d423036e0c0d8caf0c8ffc4b03
exhq <exhq@exhq.dev>
2023-06-30 15:38:05 +0330

    css is mean :(

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
 import Main from "./lib/mmain.svelte";
+import ProgressBar from "./lib/progressBar.svelte"
+
 </script>
 
 <main>
 
 <Main/>
 
+
+<h1 class="skills">my skills</h1>
+<ProgressBar value = {10} name = "gaming" />
+<ProgressBar value = {1000000} name = "shitposting" />
+<ProgressBar value = {1} name = "being useful" />
 </main>
 
+<style>
+
+</style>
 

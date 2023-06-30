4a34281b61f70d01c49159c05183c83bccd148fc
exhq <exhq@exhq.dev>
2023-06-30 15:38:14 +0330

    css is meanie :(

--- src/lib/mmain.svelte
+++ src/lib/mmain.svelte
@@ -1,18 +1,72 @@
 <script>
-    function handleKeyDown() {
-    console.log("hi");
+    let vite = "e"
+  function handleKeyDown(){
+    vite += "e"
   }
 </script>
 
-<img src="https://dp.nea.moe/avatar/712639419785412668.png" class="logo" alt="Svelte Logo" />
+
+<div class="container">
+  <div class="page1">
+    <img src="https://dp.nea.moe/avatar/712639419785412668.png" class="logo" alt="Svelte Logo" />
 <h1>ECHO</h1>
 
 <p>
-yes, this is the default <span on:click={handleKeyDown}>vite</span> template <br> yes, im working on it.
+competitive shitpost<span class="thespan" on:click={handleKeyDown}>{vite}</span>r
 </p>
+  </div>
+</div>
+
+
+
 
 
 <style>
+:root {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+  line-height: 1.5;
+  font-weight: 400;
+
+  color-scheme: light dark;
+  color: rgba(255, 255, 255, 0.87);
+  background-color: #242424;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+
+h1 {
+  font-size: 3.2em;
+  line-height: 1.1;
+}
+
+
+@media (prefers-color-scheme: light) {
+  :root {
+    color: #213547;
+    background-color: #ffffff;
+  }
+  a:hover {
+    color: #747bff;
+  }
+  button {
+    background-color: #f9f9f9;
+  }
+}
+
+.page1 {
+  height: 100vh; /* Make each page take up the full viewport height */
+  width:auto; /* Take up full width */
+  top: 0;
+  left: 0;
+}
+  .thespan { 
+    user-select: none;
+  }
   .logo {
     height: 10em;
     padding: 1.5em;
@@ -23,6 +77,18 @@ yes, this is the default <span on:click={handleKeyDown}>vite</span> template <br
   .logo:hover {
   animation: rotate 0.2s infinite linear;
 }
+
+
+.container {
+  text-align: center;
+    display: grid;
+    place-items: center;
+    height: 100vh; /* adjust to your desired height */
+  }
+
+  .centered-div {
+    text-align: center;
+  }
 
 @keyframes rotate {
   from {
---
+++ src/lib/progressBar.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let name = "";
+    export let value = 0; // The current value of the progress bar
+    export let max = 100; // The maximum value of the progress bar
+  </script>
+  
+  <style>
+    .progress-bar {
+      width: 90%;
+      background-color: transparent;
+      height: 20px;
+      overflow: hidden;
+    }
+  
+    .progress-bar-fill {
+      height: 100%;
+      background-color: dodgerblue;
+      transition: width 0.3s ease;
+    }
+  </style>
+  <h2>{name}</h2>
+
+  <div class="progress-bar">
+    <div class="progress-bar-fill" style="width: {value / max * 100}%"></div>
+  </div>
+  
